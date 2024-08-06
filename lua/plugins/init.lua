290eecdfb35877f1e8c180e186d54fc61ee7858d
Peter Lozsek <lozsek.peter@gmail.com>
2024-08-06 05:29:53 +0200

    add mapping for telescope and quit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,19 @@ return {
     end,
   },
 
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function()
+      local conf = require "nvchad.configs.telescope"
+
+      conf.defaults.mappings.i = {
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+        ["<Esc>"] = require("telescope.actions").close,
+      }
+    end
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
